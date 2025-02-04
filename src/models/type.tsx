88ed5c26e276b3b37d9dca644a261248ba102adf
indujaak <indhujaakddeveloper@gmail.com>
2025-02-04 11:10:20 +0530

    feat(ui): userCreation page creation
    basic Information and Experience section added
    
    refs:#[https://app.asana.com/0/1208796985053234/1209239944670943]

--- src/models/type.tsx
+++ src/models/type.tsx
@@ -2,3 +2,34 @@ export interface LoginForm {
   employeeID: string;
   password: string;
 }
+
+export interface CreateUserType{
+  employeeId:string
+  userId: string;
+  userName: string;
+  phoneNumber: string;
+  email: string;
+  location: string;
+  address: string;
+  password: string;
+  profileImage: string;
+  joiningDate: string;
+  previousCompany: string;
+  experienceYears: number | null; 
+  experienceMonths: number | null;
+  role:string;
+  otpPageView: boolean;
+  employeeView: boolean;
+  employeeAdd: boolean;
+  employeeEdit: boolean;
+  employeeDelete: boolean;
+  leadsView: boolean;
+  leadsAdd: boolean;
+  leadsEdit: boolean;
+  leadsDelete: boolean;
+  developerView: boolean;
+  developerAdd: boolean;
+  developerEdit: boolean;
+  developerDelete: boolean;
+  
+}
