6c5c8ba60cef38f62acadeb3aefa71fc881a99f7
pandivignesh <pandivignesh333@gmail.com>
2025-02-08 11:37:48 +0530

    feat(ui):side panel

--- src/container/pages/User/index.tsx
+++ src/container/pages/User/index.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { Box, Button } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import Sidebar from '../../../components/SlideBar/index';
+import SidePanel from '../../../components/SidePanel';
 
 import styles from './User.module.scss';
 
 const User = () => {
   const { t } = useTranslation();
-
   return (
     <Box component='main' sx={{ flexGrow: 1, p: 3, marginTop: '70px' }}>
-    
-       <Sidebar />
-       <Box
-      id='create-user'
-      data-testid='create-user'
-      className={styles.createUserButton}
-    >
-      <Button onClick={() => window.open('/create-user', '_blank')}>
-        {t('Create User')}
-      </Button>
-    </Box>
+      <SidePanel menu='User' />
+      <Box
+        id='create-user'
+        data-testid='create-user'
+        className={styles.createUserButton}
+      >
+        <Button onClick={() => window.open('/create-user', '_blank')}>
+          {t('Create User')}
+        </Button>
+      </Box>
     </Box>
   );
 };
