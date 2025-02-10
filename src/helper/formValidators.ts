4ee8cf5731815c6b19c05ce76e9d06a5ffb0bd0e
indujaak <indhujaakddeveloper@gmail.com>
2025-02-10 16:50:34 +0530

    feat(ui):  Refactor create user page
    Localization Added
    changes in choose Profile Picture Button
    
    refs:#[https://app.asana.com/0/1208796985053234/1209325671971040]

--- src/helper/formValidators.ts
+++ src/helper/formValidators.ts
@@ -9,9 +9,6 @@ export const getInputFieldErrorMessage = (error: FieldError | undefined) => {
 export const onHandleImageValidation = (value: any) => {
   const fileList = value as unknown as FileList;
   const file = fileList[0];
-  if (!fileList || fileList.length === 0) {
-    return 'Profile image is required';
-  }
   // Validate file size (max 2MB)
   if (file.size > 2 * 1024 * 1024) {
     return 'File size must be less than 2MB';
